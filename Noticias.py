2e69074b21c7d43e07005b900ec6ef5eac14bcf3
DanielHaggstrom <daniel.hpf@hotmail.com>
2020-04-28 17:33:09 +0200

    eliminado archivo obsoleto

--- Noticias.py
+++
@@ -1,9 +0,0 @@
-# este script adquirirá las noticias para el proyecto
-import requests
-import json
-
-r = requests.get("http://api.nytimes.com/svc/search/v2/articlesearch.json?q=Apple&begin_date=20100101&end_date=20151231&api-key=bZvV29QOmyUw74K0Las0bzo08O6Zihhp")
-data = r.json()
-
-with open('data.json', 'w') as outfile:
-    json.dump(data, outfile)
