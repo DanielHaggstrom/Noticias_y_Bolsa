13359f34a99a5ef51c40c135b0ed05ef61f8470c
DanielHaggstrom <daniel.hpf@hotmail.com>
2020-04-29 23:54:40 +0200

    implementar red neuronal

--- prototype.py
+++ prototype.py
@@ -9,58 +9,72 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPRegressor
 
-# AHORA ESTÁ EN MODO CLASSIFICATION
 
+# AHORA ESTÁ EN MODO REGRESIÓN
 
 # definimos una función útil para más adelante
-def make_time_steps(data, h, w, s, target="Growth"):
+def make_time_steps(data, h, w, s, targets):
+    # nuestro target es todas las columnas que terminan en '-score'
     window_values = range(1, w + 1, s)
     new_data = pandas.DataFrame()
     for name in data.columns:
         for i in window_values:
             new_col_name = '{}-{}'.format(name, i)
             new_data[new_col_name] = data[name].shift(i)
-    new_target = '{}+{}'.format(target, h)
-    new_data[new_target] = data[target].shift(-h)
-    return new_data.dropna(), new_target
+    new_targets = pandas.DataFrame()
+    for target in targets:
+        new_target = '{}+{}'.format(target, h)
+        new_targets[new_target] = data[target].shift(-h)
+    new_data.dropna(inplace=True)
+    print("Filas restantes: " + str(len(new_data)))
+    return new_data, new_targets
 
-
-# preparamos unas variables para guardar los resultados
-accuracy = []
 
 # lo primero es cargar los datos y prepar el dataframe
-path = "D:\\Coding\\PyCharm Workspace\\ProyectoBigData\\datos\\Prototipo 2\\datos_aprendizaje"
-for file in os.listdir(path):
-    data = pandas.read_csv(path + "\\" + file)
-    data.set_index("Date", inplace=True)
-    # separamos en test y training
-    training_data = data.loc["2010-01-01":"2019-12-31"]
-    testing_data = data.loc["2020-01-01":"2020-12-31"]
-    # declaramos los parámetros que vamos a usar
-    horizon = 2
-    window = 5
-    step = 1
-    data_train_shifted, target = make_time_steps(data=training_data, h=horizon, w=window, s=step)
-    data_test_shifted, _ = make_time_steps(data=testing_data, h=horizon, w=window, s=step)
-    x_train, y_train, x_test, y_test = data_train_shifted.iloc[:, :-1], \
-                                       data_train_shifted.iloc[:, -1], \
-                                       data_test_shifted.iloc[:, :-1], \
-                                       data_test_shifted.iloc[:, -1],
-    """
-    model = SVR(gamma='auto')
-    model.fit(x_train, y_train)
-    y_svr_pred = model.predict(x_test)
-    print(explained_variance_score(y_test, y_svr_pred))
-    
-    rf = RandomForestRegressor(n_estimators=1000)
-    rf.fit(x_train, y_train)
-    y_rf_pred = rf.predict(x_test)
-    r2.append(explained_variance_score(y_test, y_rf_pred))
-    """
-    rf = RandomForestClassifier()
-    rf.fit(x_train, y_train)
-    accuracy.append(accuracy_score(y_test, rf.predict(x_test)))
+path = os.path.join(os.path.dirname(__file__), "datos", "aprendizaje", "dataset.csv")
+data = pandas.read_csv(path)
+data.set_index("Date", inplace=True)
 
-plt.hist(accuracy)
-plt.show()
+# TODO normalizar datos
+
+# separamos en test y training
+training_data = data.loc["2018-07-09":"2019-12-31"]
+testing_data = data.loc["2020-01-01":"2020-12-31"]
+
+# declaramos los parámetros que vamos a usar
+horizon = 1
+window = 4
+step = 1
+
+# obtenemos las columans target y no_target
+targets = []
+for col in data.columns:
+    if "score" in col:
+        targets.append(col)
+no_targets = list(set(data.columns).difference(targets))
+
+# obtenemos los time_steps
+data_train_shifted, target = make_time_steps(training_data, horizon, window, step, targets)
+data_test_shifted, _ = make_time_steps(testing_data, horizon, window, step, targets)
+
+# volvemos a obtener target y no target para el nuevo dataset, que tiene un formato de nombres un poco diferente
+targets = []
+for col in data_train_shifted.columns:
+    if "score" in col:
+        targets.append(col)
+no_targets = list(set(data_train_shifted.columns).difference(targets))
+
+# creamos los conjuntos de entrenamiento y testeo, separados en targets y no targets
+x_train = data_train_shifted[no_targets]
+y_train = data_train_shifted[targets]
+x_test = data_train_shifted[no_targets]
+y_test = data_train_shifted[targets]
+
+# entrenamos y validamos el modelo
+reg = MLPRegressor(hidden_layer_sizes=(68,), solver="lbfgs", max_iter=10000)
+model = reg.fit(x_train, y_train)
+r2 = reg.score(reg.predict(x_test), y_test)
+print(r2)
+
